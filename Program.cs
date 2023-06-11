281d5997315104774c6d0c20c72f409c7347e181
AlexKhabibov <alex.lv.u@gmail.com>
2023-06-11 15:04:18 +0300

    Возведение числа в степень

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу полуение квадрата вводного числа
+// 1. Запросить у пользователя число, записать число в переменную
+// 2. Возвести полученное число в квадрат
+// 3. Вывести результат
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine(result);
